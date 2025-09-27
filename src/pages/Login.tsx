6794a85448b99f1cdbc6d759a8afcf626597322f
Ellen <ellenv.albuquerque@gmail.com>
2025-09-26 22:32:28 -0300

    feat: adding css to ResgisterCompany page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -39,6 +39,7 @@ export default function Login() {
     }
 
     return (
+        
         <div className="login-page">
           <div className="login-container">
             <div className="login-header">
